38c6d40556aaeaa8a2a7f98a3c7b01187819cdc2
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-06 22:51:55 +0300

    WIP Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const Wrapper = styled.footer`
+  ${tw`w-full text-center`}
+  background-color:${props => props.theme.colors.heavyMetal};
+  height: 100px;
+  color: #fff;
+`
+
+const Footer = () => (
+    <Wrapper>
+        ©RemoteShiva 2020
+    </Wrapper>
+)
+
+export default Footer
+
+
