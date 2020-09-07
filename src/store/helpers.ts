b03342dd9f2d6b2c5e23beefebf8e1e8cf6fcb12
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-08 01:17:39 +0300

    typed function to convert array to object

--- src/store/helpers.ts
+++ src/store/helpers.ts
@@ -6,3 +6,14 @@ export function typedAction<T extends string, P extends any>(
 export function typedAction(type: string, payload?: any) {
   return { type, payload };
 }
+
+type BackendObject = {
+  _id: string
+}
+
+export function arrayToObject<T extends BackendObject>(array:T[]){
+  return array.reduce((obj:{[key:string]:T}, item:T) => {
+      obj[item._id] = item
+      return obj
+  }, {})
+}
