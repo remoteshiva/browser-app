5ed86e8148d0948a43f0e6e3ab20d78747d3b663
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-08 00:09:59 +0300

    more global styles

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -1,22 +1,33 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    /* for debug */
-    /* * {
-        outline: 1px solid red !important;
-    } */
-    html {
-        min-height: 100%;
-    }
+  /* for debug */
+  /* * {
+      outline: 1px solid red !important;
+  } */
 
-    body {
-        margin: 0;
-        padding: 0;
-        font-family: Lato, Sans-Serif;
-        display: flex;
-        min-height: 100vh;
-        flex-direction: column;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0; 
+    border: 0;
+    box-sizing: border-box;
+  }
+  *:focus {
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    font-family: Lato, Sans-Serif;
+    display: flex;
+    flex-direction: column;
+    -webkit-font-smoothing: antialiased;
+  }
+  button {
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyle
