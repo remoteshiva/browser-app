cdd718cfd9d1b8edb76ee71667dfadad7b3d93eb
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-27 19:20:39 +0300

    implemented toast component

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,26 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Container, Notification } from './styles'
 
+export const getRandomId = ():string => (Math.floor((Math.random() * 10001) + 1).toString())
 
-interface Toast {
+export interface ToastModel {
+  id: string
   title: string
-  description: string
-  backgroundColor: string
-  icon: string
+  description?: string
+  icon?: string
 }
 
-
+export enum Position  {
+  tr='tr', tl='tl', br='br', bl='bl'
+}
 interface Props {
-  // toastList: []
-  position: string
+  toasts: ToastModel[]
+  position: Position
   autoDelete: boolean
-  dismissAfter: number
+  dismissAfter?: number
 }
 
-const CToast = () => {
+const ToastContainer = ({toasts, autoDelete, position, dismissAfter=5}: Props) => {
+  const [list, setList] = useState(toasts)
+
+  const deleteToast = useCallback((id:string) => {
+    const listItemIndex = list.findIndex(e => e.id === id);
+    const toastListItem = toasts.findIndex(e => e.id === id);
+    list.splice(listItemIndex, 1);
+    toasts.splice(toastListItem, 1);
+    setList([...list]);
+  }, [list, toasts])
+  
+  useEffect(() => {
+    setList([...toasts]);
+  }, [toasts]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (autoDelete && toasts.length && list.length) {
+        deleteToast(toasts[0].id);
+      }
+    }, dismissAfter*1000);
+    
+    return () => {
+        clearInterval(interval);
+    }
+  }, [toasts, autoDelete, dismissAfter, list, deleteToast]);
+
   return (
-    <>
-    </>
+    <Container className={position}>
+      {list.map(toast => (
+        <Notification key={toast.id} className={position}>
+          <div className='icon'><img src={toast.icon} alt='icon'/></div>
+          <div className='title'>{toast.title}</div>
+          <div className='description'>{toast.description}</div>
+        </Notification>
+      ))}
+    </Container>
   )
 }
 
-export default CToast
+export default ToastContainer
