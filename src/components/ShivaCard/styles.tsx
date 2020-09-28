fb8737b2a838fb8d7bdbfd18b9be20b2de76c3e8
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-28 13:12:28 +0300

    shiva card hover

--- src/components/ShivaCard/styles.tsx
+++ src/components/ShivaCard/styles.tsx
@@ -1,63 +1,68 @@
 import styled from 'styled-components'
 import tw from 'twin.macro'
+import { rgba } from 'polished'
 import { ClickOutside } from '../ClickOutside'
 
 
 export const ShivaListWrapper = styled.ul`
-    display: flex;
-    flex-flow: row wrap;
-    list-style-type:none;
+  display: flex;
+  flex-flow: row wrap;
+  list-style-type:none;
 `
 
 const BaseItemWrapper = styled.li`
-    margin-right: 40px;
-    margin-bottom: 40px;
-    width: ${props => props.theme.components.shivaCard.width};
-    height: ${props => props.theme.components.shivaCard.height};
-    border-radius: ${props => props.theme.components.shivaCard.borderRadius};
+  margin-right: 40px;
+  margin-bottom: 40px;
+  width: ${props => props.theme.components.shivaCard.width};
+  height: ${props => props.theme.components.shivaCard.height};
+  border-radius: ${props => props.theme.components.shivaCard.borderRadius};
 `
 
 export const ShivaItemWrapper = styled(BaseItemWrapper)`
-    padding-left: 21px;
-    padding-bottom: 36px;
-    box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.04);
-    background-color: #fff;
-    font-family: 'Lora';
-    p{
-        /* margin-top: 143px; */
-        font-size:34px;
-        font-weight: 400;
-    }
+  padding-left: 21px;
+  padding-bottom: 36px;
+  box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.04);
+  background-color: ${props => props.theme.colors.white};
+  font-family: 'Lora';
+  border-radius: 10px;
+  &:hover{
+    border: 1px solid ${rgba('#924623', 0.15)};
+    box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.18);
+  }
+  p{
+      font-size:34px;
+      font-weight: 400;
+  }
 `
 
 export const NewShivaItemWrapper = styled(BaseItemWrapper)` 
-    ${tw`flex items-center justify-center`}
-    border-color:${props => props.theme.colors.doveGray}; 
-    color:${props => props.theme.colors.doveGray}; 
-    border-width: 1px;
-    border-style: dashed;
-    font-size: 18px;
+  ${tw`flex items-center justify-center`}
+  border-color:${props => props.theme.colors.doveGray}; 
+  color:${props => props.theme.colors.doveGray}; 
+  border-width: 1px;
+  border-style: dashed;
+  font-size: 18px;
+  cursor: pointer;
 `
 
 export const ShivaDates = styled.div`
-    font-family: 'Lato';
-    font-size: 16px;
+  font-family: 'Lato';
+  font-size: 16px;
 `
 
 /********* menu *********/
 export const DropdownWrapper = styled(ClickOutside)`
-    ${tw`flex flex-row-reverse relative`}
-    overflow:visible;
+  ${tw`flex flex-row-reverse relative`}
+  overflow:visible;
 `
 
 export const DropdownButton = styled.span`
-    ${tw`inline-flex justify-center px-4 py-2 bg-white leading-5 focus:outline-none focus:border-blue-300 transition ease-in-out duration-150`}
-    /* ${tw`flex flex-row-reverse`} */
-    img{
-        width: 6px;
-        height: 19px;
-        object-fit: contain;
-    }
+  ${tw`inline-flex justify-center px-4 py-2 bg-white leading-5 focus:outline-none focus:border-blue-300 transition ease-in-out duration-150`}
+  img{
+      width: 6px;
+      height: 19px;
+      object-fit: contain;
+  }
 `
 
 interface DropdownContainerProps {
