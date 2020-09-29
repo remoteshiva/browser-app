b5073e57d7b59cd33c2b5110ae7c1c8d8a0aceeb
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-29 10:49:14 +0300

    refactor shiva actions

--- src/store/shiva/actions.ts
+++ src/store/shiva/actions.ts
@@ -17,40 +17,37 @@ import {
 import { shivas } from '../../mock-data'
 import { AppState } from "../";
 
+const sleep = (ms:number) => new Promise(r => setTimeout(r, ms))
 
 export const fetchShivas = () : ThunkAction<void, AppState, null, Action<string>> => async dispatch => {
-    dispatch(fetchShivaListRequest())
-    setTimeout(() => {
-        dispatch(fetchShivaListSuccess(shivas))
-    }, 1000)
+  dispatch(fetchShivaListRequest())
+  await sleep(1000)
+  dispatch(fetchShivaListSuccess(shivas))
 }
 
 export const fetchShivaById = (shivaId: string) : ThunkAction<void, AppState, null, Action<string>> => async dispatch => {
   dispatch(fetchShivaByIdRequest())
-  setTimeout(()=>{
-    const shiva = shivas.find(shiva => shiva._id === shivaId)
-    if (shiva){
-      dispatch(fetchShivaByIdSuccess(shiva))
-      dispatch(selectShiva(shiva._id))
-    } else {
-        dispatch(fetchShivaByIdError({code: 404, error: 'cannot find it'}))
-        dispatch(push('/404'))
-    }
-  },1000)
+  await sleep(1000)
+  const shiva = shivas.find(shiva => shiva._id === shivaId)
+  if (shiva){
+    dispatch(fetchShivaByIdSuccess(shiva))
+    dispatch(selectShiva(shiva._id))
+  } else {
+      dispatch(fetchShivaByIdError({code: 404, error: 'cannot find it'}))
+      dispatch(push('/404'))
+  }
 } 
 
 export const createShiva = (shiva: Shiva) : ThunkAction<void, AppState, null, Action<string>> => async dispatch => { 
   dispatch(createShivaRequest())
-  setTimeout(() => {
-    // we're generating a random id until after the firebase integration
-    const newShiva = {...shiva, _id: Math.random().toString(36).substring(3)} 
-    dispatch(createShivaSuccess(newShiva))
-  }, 1000)
+  await sleep(1000)  
+  const newShiva = {...shiva, _id: Math.random().toString(36).substring(3)} 
+  dispatch(createShivaSuccess(newShiva))
+  return newShiva
 }
 
 export const deleteShiva = (shivaId: string) : ThunkAction<void, AppState, null, Action<string>> => async dispatch => {
   dispatch(deleteShivaRequest())
-  setTimeout(() => {
-    dispatch(deleteShivaSuccess(shivaId))
-  }, 1000)
+  await sleep(1000)
+  dispatch(deleteShivaSuccess(shivaId))
 }
