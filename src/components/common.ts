2be8a44ac71a6a204b1fe5316cf34e57cb7b553b
Amit Nabarro <amit.nabarro@gmail.com>
2020-09-29 13:51:30 +0300

    common styled components

---
+++ src/components/common.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+// spacers
+interface SpaceProps {
+  height: number
+}
+export const VerticalSpace = styled.div<SpaceProps>`
+  height: ${props => `${props.height}px`};
+  width: 100%;
+`
+// buttons
+const Button = styled.button`
+  border-radius: 15px;
+  padding: 23px 85px 23px 85px;
+  text-align: center;
+  font-size: 18px;
+`
+export const LightButton = styled(Button)`
+  border: solid 1px ${props => props.theme.colors.richGold};
+  background-color: ${props => props.theme.colors.white};
+  color: ${ props => props.theme.colors.richGold };
+`
+export const DarkButton = styled(Button)`
+  background-color: ${props => props.theme.colors.richGold};
+  color: ${props => props.theme.colors.white};
+`
