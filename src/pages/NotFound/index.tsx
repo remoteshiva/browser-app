90d8ef40501063b68f3ea5ad9f5948bf208c0542
Amit Nabarro <amit.nabarro@gmail.com>
2020-10-04 15:06:31 +0300

    Not found page design

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import NotFoundImage from '../../assets/img/page-not-found.svg'
+import { Row } from '../../components/flexLayout'
+import { VerticalSpace, DarkButton } from '../../components/common'
+
+const Wrapper = styled.div`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 670px;
+    height: 270px;
+  }
+  h2 {
+    margin-top: 57px;
+    margin-bottom: 30px;
+    font-size: 43px;
+    font-family: 'Lora';
+    color: ${props => props.theme.colors.heavyMetal};
+  }
+`
 
 const NotFound = () => (
-  <>
-    <h1 style={{ fontSize: '40px' }}>404 - Not Found</h1>
-    <Link to="/">Go Home</Link>
-  </>
+  <Wrapper>
+    <VerticalSpace height={100} />
+    <Row>
+      <img src={NotFoundImage} alt="Not Found" />
+    </Row>
+    <Row>
+      <h2>Page Not Found</h2>
+    </Row>
+    <Row>
+      <p>Hmm, it seems that page does not exist.</p>
+    </Row>
+    <VerticalSpace height={40} />
+    <Row>
+      <Link to="/">
+        <DarkButton>Go Home </DarkButton>
+      </Link>
+    </Row>
+    <VerticalSpace height={200} />
+  </Wrapper>
 )
 
 export default NotFound
