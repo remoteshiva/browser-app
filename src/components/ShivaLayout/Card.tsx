233e8413f80c21ef7a870e29061670de1920afc9
Amit Nabarro <amit.nabarro@gmail.com>
2020-10-13 23:24:27 +0300

    added Cancel button and hooks to Shiva Layout card

--- src/components/ShivaLayout/Card.tsx
+++ src/components/ShivaLayout/Card.tsx
@@ -2,28 +2,52 @@ import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 import { ROLE } from '../../store/shiva/types'
 import { CardWrapper, Direction } from './styles'
-import EditIcon from '../../assets/img/edit.svg'
+import EditIcon from '../../assets/img/editWhite.svg'
 import SaveIcon from '../../assets/img/save.svg'
+import CancelIcon from '../../assets/img/cancel.svg'
 
 interface Props {
   darkMode: boolean
   role: ROLE
   editing?: boolean
-  onModeChange?: () => void
+  onModeChange: () => void
+  onCancel: () => void
   direction?: Direction
   children: ReactNode
 }
 
-const EditButton = styled.img`
+interface ButtonProps {
+  icon: string
+  iconSize: number
+}
+
+const Button = styled.button<ButtonProps>`
+  background-image: ${props => `url(${props.icon})`};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: ${props => `${props.iconSize}px`};
+  width: 30px;
+  height: 30px;
+  border-radius: 8px;
+  margin-right: 5px;
+  background-color: ${props => props.theme.colors.desertStorm};
+`
+
+const Buttons = styled.div`
   position: absolute;
   right: 10px;
   top: 10px;
 `
 
-const Card = ({ darkMode, editing, direction, role, children, onModeChange }: Props) => {
+const Card = ({ darkMode, editing, direction, role, children, onModeChange, onCancel }: Props) => {
   return (
     <CardWrapper className={darkMode && !editing ? 'darkMode' : ''} direction={direction}>
-      {role === 'Visitor' ? null : <EditButton onClick={onModeChange} src={editing ? SaveIcon : EditIcon} />}
+      {role === 'Visitor' ? null : (
+        <Buttons>
+          {editing ? <Button icon={CancelIcon} onClick={onCancel} iconSize={14} /> : null}
+          <Button onClick={onModeChange} icon={editing ? SaveIcon : EditIcon} iconSize={17} />
+        </Buttons>
+      )}
       {children}
     </CardWrapper>
   )
