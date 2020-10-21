a69d88364b8d6906995ad1ed30985604ca3ee018
Amit Nabarro <amit.nabarro@gmail.com>
2020-10-21 11:34:11 +0300

    add, update and delete shiva visits

---
+++ src/store/shiva/helpers.ts
@@ -0,0 +1,41 @@
+import { startOfDay, addDays } from 'date-fns'
+import { Shiva, Visit } from './types'
+
+const generateRandomKey = (): string => {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+/**
+ * @description Initializes a new Shiva object with default params.
+ * @param shiva - Optional partial shiva model for overriding defaults
+ * @example - const shiva = initializeShiva({nameOfDeceased: 'John Doe'})
+ */
+export const initializeShiva = (newShiva?: Partial<Shiva>): Shiva => ({
+  id: '',
+  nameOfDeceased: '',
+  startDate: startOfDay(new Date()),
+  endDate: startOfDay(addDays(new Date(), 7)),
+  message: '',
+  mourners: [],
+  videoLink: null,
+  mournerKey: generateRandomKey(),
+  visitorKey: generateRandomKey(),
+  visits: {},
+  titleImage: null,
+  about: '',
+  images: [],
+  mealSignups: '',
+  minianTimes: '',
+  donations: '',
+  inviteMessage: '',
+  ...newShiva,
+})
+
+export const initializeVisit = (visit?: Partial<Visit>): Visit => ({
+  id: generateRandomKey(),
+  date: new Date(),
+  length: 0,
+  mourners: [],
+  visitors: [],
+  ...visit,
+})
