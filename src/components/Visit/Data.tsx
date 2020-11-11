74b27afc865f8e9fc0570be0c0dd749a6d51b4c0
Amit Nabarro <amit.nabarro@gmail.com>
2020-11-11 13:36:20 +0200

    toggle mourner visit participation

--- src/components/Visit/Data.tsx
+++ src/components/Visit/Data.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Visit, Visitor, Mourner } from '../../store/shiva/types'
+import { Visit, Visitor, Mourner, MournerId, Role } from '../../store/shiva/types'
 import CheckedIcon from '../../assets/img/checkbox-round-checked.svg'
 import UncheckedIcon from '../../assets/img/checkbox-round-unchecked.svg'
 import { VerticalSpace } from '../../components/common'
-import { CalendarMode } from '../types'
 
 
 const Wrapper = styled.div`
@@ -22,7 +21,17 @@ const Wrapper = styled.div`
     color: ${props=> props.theme.colors.heavyMetal};
   }
   .mourner{
-    /* margin-top: 10px; */
+    display: flex;
+    &.editor{
+      margin-top: 10px;
+      min-height: 20px;
+      span{
+        line-height: 20px;
+      }
+    }
+    img{
+      margin-right: 5px;
+    }
     .name{
       display: inline;
       margin-right: 14px;
@@ -51,16 +60,27 @@ const Wrapper = styled.div`
 `
 
 interface MournerRowProps extends Mourner {
-  mode: CalendarMode
+  attending: boolean
+  role: Role
+  onCheckChanged: (identifier: string, attending: boolean)=>void
 }
-const MournerRow = ({name, relationship, mode}: MournerRowProps) => {
+const MournerRow = ({name, relationship, attending, role, onCheckChanged}: MournerRowProps) => {
+  const [checked, setChecked] = useState(attending)
+  const handleChange = () => {
+    if(onCheckChanged)
+      onCheckChanged(name+relationship, !checked)
+    setChecked(!checked)
+  }
+  if(role==='Visitor' && !attending)
+    return null
   return(
-    <div className='mourner'>
-      {mode === 'Edit' ? <img src={CheckedIcon} alt='checkbox'/> : null }
+    <div className={`mourner ${role === 'Visitor' ? '' : 'editor'}`}>
+      {role !== 'Visitor' ? <img src={checked ? CheckedIcon: UncheckedIcon} onClick={handleChange} alt='checkbox'/> : null }
       <span className='name'>{name}</span>
       <span className='relationship'>{relationship}</span>
     </div>
   )
+
 }
 
 const VisitorRow = ({name, time}: Visitor) => (
@@ -70,20 +90,24 @@ const VisitorRow = ({name, time}: Visitor) => (
 )
 
 interface Props extends Visit {
-  mode: CalendarMode
+  role: Role
   mournersList: Mourner[]
   onAddVisitor: ()=>void
+  onToggleMournerParticipation: (mourner: MournerId, attending: boolean)=>void
 }
 
-const VisitData = ({mode, mournersList, visitors, mourners, onAddVisitor}: Props) => {
+const VisitData = ({role, mournersList, visitors, missingMourners, onAddVisitor, onToggleMournerParticipation}: Props) => {
+
   const renderMourners = () => {
-    let list: Mourner[]
-    if(mode==='Edit'){
-      list = mournersList
-    }else{
-      list = mournersList
-    }
-    return list.map((m,i) => <MournerRow key={i} mode={mode} {...m}/>)
+    return mournersList.map((m,i) =>
+      <MournerRow
+        key={i}
+        role={role}
+        attending={!missingMourners.includes(m.name+m.relationship)}
+        {...m}
+        onCheckChanged={onToggleMournerParticipation}
+      />
+    )
   }
   return (
     <Wrapper>
