b53a74da6841e642966fa200f647b6bfc5c67273
Andrew Ash <andrew@emotiveapps.com>
2021-01-18 21:11:59 -0600

    “visitor_add” emails are sent to the correct email address
    Previously the “to” email was always undefined on this message, resulting in an error. I’ve also added comments explaining the architecture for how these emails are sent. If I’ve got this right, the message will send but some of the properties like nameOfDeceased will be wrong.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -10,15 +10,21 @@ const mailgun = require('mailgun-js')({
 
 admin.initializeApp(functions.config().firebase);
 
+/// This email is sent when a new document is added to the visit_messages Firestore collection. See addVisitorMessage()
+/// The actual body of the email is specified by the template, which is stored on mailgun.
+//  For reference, you can edit and debug templates using the standalone NodeJS application Amit built in the /mailgun directory
 exports.sendVisitorEmail = functions.firestore.document(`visitor_messages/{mailId}`).onCreate(async (snapshot, context) => {
   const visitorMessage = snapshot.data();
+  const visitDay = "Tuesday";
+  const visitDate = visitorMessage.visitor.time;
+  const videoLink = "zoom.us/123456";
+  const nameOfDeceased = "John Doe";
   const data = {
-      from: 'noreply@remoteshiva.org',
-      to: visitorMessage.email,
-      template: visitorMessage.template,
-      subject: 'visitor message',
-      text: visitorMessage.text,
-      html: visitorMessage.html,
+      from: 'RemoteShiva <info@remoteshiva.org>',
+      to: visitorMessage.visitor.email,
+      template: visitorMessage.templateName,
+      subject: 'Your shiva visit is confirmed',
+      'h:X-Mailgun-Variables': `{"title": "Your shiva visit is confirmed", "day": "${visitDay}", "date": "${visitDate}", "visitorUrl": "http://app.removeshiva.org/v/${visitId}", "videoLink": "${videoLink}", "nameOfDeceased": "${nameOfDeceased}", "visitor": "${visitorMessage.visitor}"}`
   };
   mailgun.messages().send(data, (error, body) => {
           console.log(body);
