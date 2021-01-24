882d35e170e1fbbe528fca8425a5799c461ebb26
Andrew Ash <andrew@emotiveapps.com>
2021-01-24 12:25:34 -0600

    Added 3 other email templates

--- mailgun/index.js
+++ mailgun/index.js
@@ -20,12 +20,44 @@ app.get('/add_visitor', (req, res) => {
   res.render('add_visitor',  {
     layout: false,
     title : 'oy mh god',
+    visitorName: 'Ron Burgundy',
     day: 'Tuesday',
-    date: 'Tuesday, December 28 1:00 PM',
-    visitorUrl: 'http://app.removeshiva.org/v/445erhgdhj',
+    date: 'December 28th, 2021 at 1:00 PM',
+    visitorUrl: 'https://app.removeshiva.org/v/445erhgdhj',
     videoLink: 'zoom.us/123456',
+    nameOfDeceased: 'Brian Fantana'
+  });
+});
+
+app.get('/new_user', (req, res) => {
+  res.render('new_user',  {
+    layout: false,
+    title : 'Welcome to RemoteShiva',
+    organizerName: 'Jake Tapper',
+    dashboardUrl: 'https://app.removeshiva.org/'
+  });
+});
+
+app.get('/timeslot_deleted', (req, res) => {
+  res.render('timeslot_deleted',  {
+    layout: false,
+    title : 'Shiva time change - can you reschedule?',
+    visitorName: 'Ron Burgundy',
     nameOfDeceased: 'Brian Fantana',
-    visitorName: 'Ron Burgundy'
+    visitorUrl: 'https://app.removeshiva.org/'
+  });
+});
+
+app.get('/visit_upcoming', (req, res) => {
+  res.render('visit_upcoming',  {
+    layout: false,
+    title : 'You have a shiva visit tomorrow',
+    visitorName: 'Ron Burgundy',
+    day: 'Tuesday',
+    date: 'December 28th, 2021 at 1:00 PM',
+    visitorUrl: 'https://app.removeshiva.org/v/445erhgdhj',
+    videoLink: 'zoom.us/123456',
+    nameOfDeceased: 'Brian Fantana'
   });
 });
 
