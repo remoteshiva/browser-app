b9dd8d759dad69b7cc207b31bc1831127482e9e2
Kristin Ruben <kruben@gmail.com>
2021-01-24 14:24:04 -0500

    Mostly formatting

--- src/components/Calendar/context.tsx
+++ src/components/Calendar/context.tsx
@@ -1,21 +1,46 @@
-import React, { FunctionComponent, createContext } from 'react'
-import { Role } from '../../store/shiva/types'
-import { CalendarMode } from '../types'
+import React, { FunctionComponent, createContext } from 'react';
+import { Role } from '../../store/shiva/types';
+import { CalendarMode } from '../types';
 export interface CalendarContextProps {
-  startHour: number
-  endHour: number
-  mode: CalendarMode
-  role: Role
+  startHour: number;
+  endHour: number;
+  mode: CalendarMode;
+  role: Role;
 }
 
-const CalendarContext = createContext<CalendarContextProps>({ startHour: 0, endHour: 0, mode: 'View', role: 'Visitor' })
+const CalendarContext = createContext<CalendarContextProps>({
+  startHour: 0,
+  endHour: 0,
+  mode: 'View',
+  role: 'Visitor',
+});
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export const CalendarProvider: FunctionComponent<CalendarContextProps> = ({ startHour, endHour, mode, role, children }) => (
-  <CalendarContext.Provider value={{ startHour, endHour, mode, role }}>{children}</CalendarContext.Provider>
-)
+export const CalendarProvider: FunctionComponent<CalendarContextProps> = ({
+  startHour,
+  endHour,
+  mode,
+  role,
+  children,
+}) => (
+  <CalendarContext.Provider value={{ startHour, endHour, mode, role }}>
+    {children}
+  </CalendarContext.Provider>
+);
 
-export const withCalendarContext = <P extends CalendarContextProps>(WrapperComponent: React.ComponentType<P>): FunctionComponent<Omit<P, keyof CalendarContextProps>> => props => (
-  <CalendarContext.Consumer>{({ startHour, endHour, mode, role }) => <WrapperComponent {...(props as P)} mode={mode} role={role} startHour={startHour} endHour={endHour} />}</CalendarContext.Consumer>
-)
+export const withCalendarContext = <P extends CalendarContextProps>(
+  WrapperComponent: React.ComponentType<P>
+): FunctionComponent<Omit<P, keyof CalendarContextProps>> => props => (
+  <CalendarContext.Consumer>
+    {({ startHour, endHour, mode, role }) => (
+      <WrapperComponent
+        {...(props as P)}
+        mode={mode}
+        role={role}
+        startHour={startHour < endHour ? startHour : endHour}
+        endHour={endHour > startHour ? endHour : startHour}
+      />
+    )}
+  </CalendarContext.Consumer>
+);
--- src/components/Calendar/rulers.tsx
+++ src/components/Calendar/rulers.tsx
@@ -1,34 +1,59 @@
-import React from 'react'
-import { eachDayOfInterval } from 'date-fns'
-import { HRulerWrapper, VRulerWrapper, Day, Hour } from './styles'
-import { withCalendarContext, CalendarContextProps } from './context'
+import React from 'react';
+import { eachDayOfInterval } from 'date-fns';
+import { HRulerWrapper, VRulerWrapper, Day, Hour } from './styles';
+import { withCalendarContext, CalendarContextProps } from './context';
 
 interface HorizontalRulerProps extends CalendarContextProps {
-  startDate: Date
-  endDate: Date
+  startDate: Date;
+  endDate: Date;
 }
-export const HorizontalRuler = withCalendarContext(({ startDate, endDate }: HorizontalRulerProps) => {
-  const days = eachDayOfInterval({ start: startDate, end: endDate })
-  return (
-    <HRulerWrapper numOfColumns={days.length}>
-      {days.map((day, i) => (
-        <Day key={i}>{day.toLocaleDateString(undefined, { weekday: 'short', month: 'short', day: 'numeric' })}</Day>
-      ))}
-    </HRulerWrapper>
-  )
-})
+export const HorizontalRuler = withCalendarContext(
+  ({ startDate, endDate }: HorizontalRulerProps) => {
+    const days = eachDayOfInterval({ start: startDate, end: endDate });
+    return (
+      <HRulerWrapper numOfColumns={days.length || 7}>
+        {days.map((day, i) => (
+          <Day key={i}>
+            {day.toLocaleDateString(undefined, {
+              weekday: 'short',
+              month: 'short',
+              day: 'numeric',
+            })}
+          </Day>
+        ))}
+      </HRulerWrapper>
+    );
+  }
+);
 
 interface VerticalRulerProps extends CalendarContextProps {}
 
-export const VerticalRuler = withCalendarContext(({ startHour, endHour }: VerticalRulerProps) => {
-  const hours = Array.from({ length: endHour - startHour + 1 }, (_, i) => (i + startHour > 11 ? `${i + startHour - 12}pm` : `${i + startHour}am`)) //`${i + startHour > 12 ? ${i + startHour}'pm' : 'am'}`)
-    .map(h => (h === '0pm' ? 'Noon' : h))
-    .map(h => (h === '12pm' ? 'Midnight' : h))
-  return (
-    <VRulerWrapper>
-      {hours.map((hour, i) => (
-        <Hour key={i}>{hour}</Hour>
-      ))}
-    </VRulerWrapper>
-  )
-})
+export const VerticalRuler = withCalendarContext(
+  ({ startHour, endHour }: VerticalRulerProps) => {
+    let adjustedStartHour = 0;
+    let adjustedEndHour = 0;
+    if (startHour > endHour) {
+      adjustedEndHour = startHour;
+      adjustedStartHour = endHour;
+    } else {
+      adjustedStartHour = startHour;
+      adjustedEndHour = endHour;
+    }
+    const hours = Array.from(
+      { length: adjustedEndHour - adjustedStartHour + 1 },
+      (_, i) =>
+        i + adjustedStartHour > 11
+          ? `${i + adjustedStartHour - 12}pm`
+          : `${i + adjustedStartHour}am`
+    ) //`${i + startHour > 12 ? ${i + startHour}'pm' : 'am'}`)
+      .map(h => (h === '0pm' ? 'Noon' : h))
+      .map(h => (h === '12pm' ? 'Midnight' : h));
+    return (
+      <VRulerWrapper>
+        {hours.map((hour, i) => (
+          <Hour key={i}>{hour}</Hour>
+        ))}
+      </VRulerWrapper>
+    );
+  }
+);
