fdd3721e3899284499f6b0f12dfc7ce142d81ab6
Kristin Ruben <kruben@gmail.com>
2021-01-31 17:02:34 -0500

    Add beta to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import tw from 'twin.macro'
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 const Wrapper = styled.footer`
   ${tw`w-full text-center`}
   background-color:${props => props.theme.colors.heavyMetal};
-  height: 100px;
+  height: 120px;
   color: #fff;
-`
+`;
 
-const Footer = () => <Wrapper style={{ paddingTop: '2rem' }}>© RemoteShiva 2020-2021 <br/> This initiative was made possible through ROI Community's Coronavirus Response Micro Grant Program.</Wrapper>
+const Footer = () => (
+  <Wrapper style={{ paddingTop: '2rem' }}>
+    © RemoteShiva 2020-2021
+    <br />
+    RemoteShiva is currently in beta. If you encounter any issues, please
+    contact info@remoteshiva.org.
+    <br />
+    This initiative was made possible through ROI Community's Coronavirus
+    Response Micro Grant Program.
+  </Wrapper>
+);
 
-export default Footer
+export default Footer;
