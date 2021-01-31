c11fe5c02b956fe260183379906444b8aa3051aa
Kristin Ruben <kruben@gmail.com>
2021-01-31 16:37:26 -0500

    Back out of video link changes, fix blurry favicon

--- src/templates/Shiva/VideoLink.tsx
+++ src/templates/Shiva/VideoLink.tsx
@@ -10,45 +10,27 @@ const VideoLink = ({ shiva, editing, save }: ShivaPanel) => {
   const [videoLink, setVideoLink] = useState(
     shiva.videoLink ? shiva.videoLink.toString() : ''
   );
-  const linkFromStore = shiva.videoLink?.href;
   const [validURL, setValidURL] = useState(isValidURL(videoLink));
   const dispatch = useDispatch();
   useEffect(() => {
     if (save && save > 0) {
-      if (linkFromStore && isValidURL(linkFromStore) && isValidURL(videoLink)) {
+      if (isValidURL(videoLink)) {
         setValidURL(true);
-        const partialShiva = {
-          videoLink: new URL(videoLink),
-        };
+        const partialShiva = { videoLink: new URL(videoLink) };
         dispatch(patchShiva(shiva.id, partialShiva));
-        return;
-      } else if (!isValidURL(videoLink)) {
-        // TODO: wow, I'm really sorry
-        const newURL = videoLink.split('</a>')[1];
-        const currentHref = videoLink.split('href="')[1]
-          ? videoLink.split('href="')[1].split('"')[0]
-          : '';
-        if (newURL !== undefined) {
-          setVideoLink(currentHref + newURL);
-          setValidURL(true);
-          const partialShiva = {
-            videoLink: new URL(currentHref + newURL),
-          };
-          dispatch(patchShiva(shiva.id, partialShiva));
-        }
       } else {
         setValidURL(false);
       }
     }
-  }, [dispatch, linkFromStore, save, shiva.id, videoLink]);
-  const link = videoLink?.toString() || '';
+  }, [dispatch, save, shiva.id, videoLink]);
 
+  const link = videoLink?.toString() || '';
   return (
     <>
       <h2>Video link</h2>
       <Editable
         tagName="a"
-        href={videoLink}
+        href={link}
         className={
           'video-link ' + (editing ? 'active' : validURL ? '' : 'invalid')
         }
