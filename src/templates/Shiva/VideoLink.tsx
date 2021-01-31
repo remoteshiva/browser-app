fbbfa9c6df53cffaec463437af5e2f0efba83838
Kristin Ruben <kruben@gmail.com>
2021-01-31 13:40:53 -0500

    Fix video link editing

--- src/templates/Shiva/VideoLink.tsx
+++ src/templates/Shiva/VideoLink.tsx
@@ -1,45 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { patchShiva } from '../../services/shiva'
-import { ShivaPanel, withPanel } from './Panel'
-import Editable from '../../components/Editable'
-import { isValidURL } from '../../utils'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { patchShiva } from '../../services/shiva';
+import { ShivaPanel, withPanel } from './Panel';
+import Editable from '../../components/Editable';
+import { isValidURL } from '../../utils';
 
 const VideoLink = ({ shiva, editing, save }: ShivaPanel) => {
-  const instructions = `Add link here`
-  const [videoLink, setVideoLink] = useState(shiva.videoLink ? shiva.videoLink.toString() : '')
-  const [validURL, setValidURL] = useState(isValidURL(videoLink))
-  const dispatch = useDispatch()
+  const instructions = `Add link here`;
+  const [videoLink, setVideoLink] = useState(
+    shiva.videoLink ? shiva.videoLink.toString() : ''
+  );
+  const [validURL, setValidURL] = useState(isValidURL(videoLink));
+  const dispatch = useDispatch();
   useEffect(() => {
     if (save && save > 0) {
       if (isValidURL(videoLink)) {
-        setValidURL(true)
-        const partialShiva = { videoLink: new URL(videoLink) }
-        dispatch(patchShiva(shiva.id, partialShiva))
+        setValidURL(true);
+        setVideoLink(videoLink);
+        const partialShiva = { videoLink: new URL(videoLink) };
+        dispatch(patchShiva(shiva.id, partialShiva));
       } else {
-        setValidURL(false)
+        setValidURL(false);
       }
     }
-  }, [dispatch, save, shiva.id, videoLink])
-  // const handleInput = (html: string) => {
-  //   try {
-  //     const url = new URL(html).toString()
-  //     setVideoLink(url)
-  //   } catch (error) {}
-  // }
-  const link = videoLink?.toString() || ''
+  }, [dispatch, save, shiva.id, videoLink]);
+  const link = videoLink?.toString() || '';
   return (
     <>
       <h2>Video link</h2>
       <Editable
         tagName="a"
         href={link}
-        className={'video-link ' + (editing ? 'active' : validURL ? '' : 'invalid')}
+        className={
+          'video-link ' + (editing ? 'active' : validURL ? '' : 'invalid')
+        }
         html={link ? link : editing ? '' : instructions}
         active={editing || false}
         onInput={(html: string) => setVideoLink(html)}
       />
     </>
-  )
-}
-export default withPanel(VideoLink)
+  );
+};
+export default withPanel(VideoLink);
