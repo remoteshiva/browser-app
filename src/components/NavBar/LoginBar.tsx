1ba8d0d552cb55ba49aca69e300f1df01af22f69
Kristin Ruben <kruben@gmail.com>
2021-01-31 20:42:36 -0500

    Remove hover state

--- src/components/NavBar/LoginBar.tsx
+++ src/components/NavBar/LoginBar.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { push } from 'connected-react-router'
-import styled from 'styled-components'
-import tw from 'twin.macro'
-import * as Routes from '../../routes'
-import { MenuItem } from './styles'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import * as Routes from '../../routes';
+import { MenuItem } from './styles';
 
 const LoginButton = styled.button`
   ${tw`text-white font-bold py-2 px-4 rounded`}
   background-color: ${props => props.theme.colors.richGold};
-`
+`;
 
 const LoginBar = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <ul className="flex flex-row">
       <MenuItem>
@@ -22,7 +22,9 @@ const LoginBar = () => {
         <a href="https://www.remoteshiva.org/tips">Tips</a>
       </MenuItem>
       <MenuItem>
-        <a href="https://www.remoteshiva.org/example-shiva">Sample RemoteShiva</a>
+        <a href="https://www.remoteshiva.org/example-shiva">
+          Sample RemoteShiva
+        </a>
       </MenuItem>
       <MenuItem>
         <a href="https://blog.remoteshiva.org/index.php/faq/">FAQ</a>
@@ -30,11 +32,11 @@ const LoginBar = () => {
       <MenuItem>
         <a href="https://blog.remoteshiva.org/">Blog</a>
       </MenuItem>
-      <MenuItem>
-        <LoginButton onClick={() => dispatch(push(Routes.LOGIN_PAGE))}>Log in</LoginButton>
-      </MenuItem>
+      <LoginButton onClick={() => dispatch(push(Routes.LOGIN_PAGE))}>
+        Log in
+      </LoginButton>
     </ul>
-  )
-}
+  );
+};
 
-export default LoginBar
+export default LoginBar;
